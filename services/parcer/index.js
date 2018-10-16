af95f72aa4e2c965d71fe1a3b06137337881fb64
Prilipko Oleg <prilipkoleg@gmail.com>
2018-10-16 22:42:32 +0300

    added ability to get data by channel id or user name

--- services/parcer/index.js
+++ services/parcer/index.js
@@ -6,33 +6,60 @@ const config = require('../../config').parser;
 class MainParser {
 
   constructor(api) {
+    this.api = api;
     this.ChannelParser = new Channel(api);
     this.VideoParser = new Video(api);
   }
 
-  start(channelId) {
+  start(channelIdOrUserName) {
 
-    return Promise.all([
-      this.ChannelParser.getData(channelId),
-      this.VideoParser.getData(channelId)
-    ])
+    return this.getChannelId(channelIdOrUserName)
+      .then((channelId) => Promise.all([
+        this.ChannelParser.getData(channelId),
+        this.VideoParser.getData(channelId)
+      ]))
       .then(([channelInfo, videosInfo]) => {
         const data = videosInfo.map(item => Object.assign({}, channelInfo, item));
 
         const XlsxWriter = new writers.Xlsx();
         const CsvWriter = new writers.Csv();
 
-        XlsxWriter.write(data, channelId);
+        XlsxWriter.write(data, channelIdOrUserName);
         CsvWriter.writeIntoCommon(data);
 
         return data;
       })
       .catch(error => console.error(
-        `Channel '${channelId}' error:`,
+        `Channel '${channelIdOrUserName}' error:`,
         //error
       ));
   }
 
+  getChannelId(channelIdOrUserName) {
+    return new Promise((resolve, reject) => {
+      const defaultOptions = {part: 'snippet', maxResults: 1};
+
+      return this.api.channels.list(Object.assign({}, {id: channelIdOrUserName}, defaultOptions))
+        .then((response) => {
+          const { status, headers, data } = response;
+          if (data && data.items && !data.items.length) {
+            return Promise.reject('This is Not channelId');
+          }
+          resolve(channelIdOrUserName);
+        })
+        .catch(error => {
+          this.api.channels.list(Object.assign({}, {forUsername: channelIdOrUserName}, defaultOptions))
+            .then(response => {
+              const { status, headers, data } = response;
+              const item = data && data.items && data.items[0];
+              const channelId = item && item.id;
+
+              return resolve(channelId) || reject('Channel not valid')
+            })
+            .catch(reject);
+        })
+    });
+  }
 
 }
 
